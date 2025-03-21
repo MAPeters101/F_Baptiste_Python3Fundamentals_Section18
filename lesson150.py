a8f464c240d4a3ae36d5bb338a6c3ea37418b403
Mark Peters <mapeters101@earthlink.net>
2025-03-21 17:44:40 -0400

    Work on lesson150 from 1:00 to 2:35.

--- lesson150.py
+++ lesson150.py
@@ -4,4 +4,16 @@ def wrapper(func):
         return result
     return inner
 
+def add(a, b, c):
+    return a+b+c
 
+def greet(name):
+    return f'Hello {name}'
+
+def join(data, *, item_sep=',', line_sep='\n'):
+    return line_sep.join(
+        [
+            item_sep.join(str(item) for item in row)
+            for row in data
+        ]
+    )
