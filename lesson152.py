260e14fe3a7ec7a65ffff77601d1dad4044ee9c8
Mark Peters <mapeters101@earthlink.net>
2025-03-22 19:07:56 -0400

    Work on lesson152 from 2:10 to 5:00.

--- lesson152.py
+++ lesson152.py
@@ -5,6 +5,31 @@ def cache(func):
         return result
     return inner
 
+cache_dict = {}
+def cache(func):
+    def inner(*args):
+        if args in cache_dict:
+            return cache_dict[args]
+        else:
+            result = func(*args)
+            cache_dict[args] = result
+            return result
+    return inner
+
+@cache
+def add(a, b):
+    return a+b
+
+@cache
+def mult(a, b):
+    return a*b
+
+print(add(1,2))
+print(cache_dict)
+
+print(mult(1,2))
+print(cache_dict)
+print('-'*80)
 
 
 
